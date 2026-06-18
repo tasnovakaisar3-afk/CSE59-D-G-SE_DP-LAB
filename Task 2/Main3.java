8335861d0b1b019518ec3a4cf154c8df82f4203c
tasnovakaisar3-afk <tasnovakaisar3@gmail.com>
2026-06-18 17:00:44 +0600

    Update Main3.java

--- Task 2/Main3.java
+++ Task 2/Main3.java
@@ -1,1 +1,17 @@
+class Countdown {
 
+    void start() {
+        for (int i = 5; i >=1; i--){
+            System.out.println(i);
+        }
+        System.out.println(x: "GO!");
+
+    }
+}
+
+public class Main3 {
+    public statics void main(String[] args){
+        Countdown countdown = new Countdown();
+        countdown.start();
+    }
+}
