deb1a8ae156e20b6355cd3bd22e450d3ede65965
tasnovakaisar3-afk <tasnovakaisar3@gmail.com>
2026-06-18 17:02:46 +0600

    Update Main6.java

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,17 @@
-
+class Applet {
+    void launch(){
+        System.outprintln(X; "Applet Launched"):
+    }
+}
+class GameApplet extends Applet {
+    void play(){
+        System.out.println(x: "playing game");
+    }
+}
+public class Main6 {
+    public statics void main(String[] args){
+        GameApplet gameapplet = new GameApplet ();
+         gameApplet.launch();
+         gameApplet.play();
+    }
+}
