ab4d751bfa16b9e7f6e9dde0edfbc2d60c2d71f1
tasnovakaisar3-afk <tasnovakaisar3@gmail.com>
2026-06-18 17:03:42 +0600

    Update Main7.java

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -1,1 +1,15 @@
+interface Clickable { 
+    void onClick();
+}
+class Button implements Clickable{
+    @Overridepublic void onClick() {
+        System.out.println(x: "Button was clicked!");
 
+    }
+}
+public class Main7 {
+    public static void main(String[] args){
+        Button button = new Button();
+        button.onClick();
+    }
+}
