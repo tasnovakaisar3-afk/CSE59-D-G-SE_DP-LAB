bfb519e2501cb75a6a4b35ffae2591b159008b96
tasnovakaisar3-afk <tasnovakaisar3@gmail.com>
2026-06-25 16:58:49 +0600

    Update Main1.java

--- Task 3/Main1.java
+++ Task 3/Main1.java
@@ -1,1 +1,40 @@
 
+package com.mycompany.main;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+       User user = new User("Rahim","rahim@gmail.com");
+       
+       if(EmailValidator .isValid(user.getEmail())){
+       } else {
+           UserRepository repository = new UserRepository();
+           repository.save(user);
+        }
+    }
+}
+class User{
+    
+    public User(String username, String email){
+    }
+}
+
+class EmailValidator{
+    
+    public static boolean isValid(String email){
+        if (email !=null && email.contains("@"));
+        return true;
+    }
+     
+
+
+class UserRepository {
+     
+    public void save(User user){
+        System.out.println("Connecting to database...");
+        System.out.println(
+        "Saving user " + user.getUsername() + " to the users table."
+        );
+    }
+ }
