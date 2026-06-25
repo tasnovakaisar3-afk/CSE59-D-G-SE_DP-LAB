f4d1d602a0724890625684432a493505ed889c0a
tasnovakaisar3-afk <tasnovakaisar3@gmail.com>
2026-06-25 16:59:29 +0600

    Update Main2.java

--- Task 3/Main2.java
+++ Task 3/Main2.java
@@ -1,1 +1,55 @@
 
+package com.mycompany.mavenproject3;
+
+
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+
+        Shape[] shapes = {
+            new Rectangle(10, 5),
+            new Circle(7)
+        };
+
+        AreaCalculator calculator = new AreaCalculator();
+
+        System.out.println(
+            "Total Area: " + calculator.calculateTotalArea(shapes)
+        );
+    }
+}
+
+
+interface Shape {
+    double calculateArea();
+}
+
+
+class Rectangle implements Shape {
+    private final double length;
+    private final double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+
+class Circle implements Shape {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
